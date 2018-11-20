5aa377180ffb4af3d8f73337c9ff90be8a5bafd5
Chronium <valdiadGrom@yandex.ru>
2018-11-20 10:18:10 +0300

    Добавьте файлы проекта.

---
+++ lab10/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace lab10
+{
+    partial class Плеер
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Play = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.volumeBar1 = new System.Windows.Forms.TrackBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(38, 21);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(58, 25);
+            this.Play.TabIndex = 0;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(102, 21);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Pause";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 24);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // volumeBar1
+            // 
+            this.volumeBar1.Location = new System.Drawing.Point(246, 21);
+            this.volumeBar1.Name = "volumeBar1";
+            this.volumeBar1.Size = new System.Drawing.Size(104, 37);
+            this.volumeBar1.TabIndex = 7;
+            this.volumeBar1.Value = 10;
+            this.volumeBar1.Scroll += new System.EventHandler(this.volumeBar1_Scroll);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 96);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(338, 37);
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(254, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Volume";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Track Bar";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 165);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(546, 82);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Track List";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(395, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Запись в файл";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Плеер
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 247);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.volumeBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Плеер";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Плеер";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar volumeBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ lab10/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace lab10
+{
+   
+    public partial class Плеер : Form
+    {
+        WindowsMediaPlayer wmp = new WindowsMediaPlayer();
+        double cp = 0;
+        List<Track> PlayList = new List<Track>();
+        public Плеер()
+        {
+            InitializeComponent();
+            volumeBar1.Value = 10;
+            Play.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            wmp.settings.volume = volumeBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((cp > 0)&& (wmp.URL == PlayList[listBox1.SelectedIndex].Way))
+            {
+                wmp.controls.play();
+                wmp.controls.currentPosition = cp;
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = true;
+                wmp.URL = PlayList[listBox1.SelectedIndex].Way;
+                wmp.controls.play();
+                //listBox1.Items[listBox1.SelectedIndex] += "   " + PlayList[listBox1.SelectedIndex].Duration;
+                //PlayList[listBox1.SelectedIndex].Duration = Convert.ToInt32(wmp.currentMedia.duration);
+                //listBox1.Items[listBox1.SelectedIndex] = PlayList[listBox1.SelectedIndex].Name +
+                //     "   " + PlayList[listBox1.SelectedIndex].Duration;
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            wmp.controls.pause();
+            timer1.Enabled = false;
+            cp = wmp.controls.currentPosition;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wmp.controls.stop();
+            cp = 0;
+        }
+
+        private void volumeBar1_Scroll(object sender, EventArgs e)
+        {
+            wmp.settings.volume = volumeBar1.Value;
+
+            toolTip1.SetToolTip(volumeBar1, volumeBar1.Value.ToString());
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wmp.controls.currentPosition = trackBar1.Value;
+
+            toolTip1.SetToolTip(trackBar1, wmp.controls.currentPositionString);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+            trackBar1.Value = Convert.ToInt32(wmp.controls.currentPosition);
+            int dur =  Convert.ToInt32(wmp.currentMedia.duration);
+            int durMin = dur / 60;
+            int durSek = dur % 60;
+
+            PlayList[listBox1.SelectedIndex].Duration = durMin.ToString() + "," + durSek.ToString() + " Мин.";
+
+            listBox1.Items[listBox1.SelectedIndex] = PlayList[listBox1.SelectedIndex].Name +
+                 "  |  " + PlayList[listBox1.SelectedIndex].Duration;
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if ((openFileDialog1.ShowDialog() == DialogResult.OK))
+            {
+                listBox1.Items.Add(Path.GetFileName(openFileDialog1.FileName));
+                PlayList.Add(new Track() { Way = openFileDialog1.FileName ,
+                                           Name = Path.GetFileName(openFileDialog1.FileName)});
+
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            Play.Enabled = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label3.Text = "Список треков если по русски";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Громкость";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.IsReadOnly)
+            {
+
+            }
+        }
+    }
+
+    class Track
+    {
+        public string Name { get; set; }
+        public string Way { get; set; }
+        public string Duration { get; set; }
+        
+    }
+}
+
